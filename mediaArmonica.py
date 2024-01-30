53cd6673532b7f5db0d135857fef22be12dbc625
ElJofator <79776204+ElJofator@users.noreply.github.com>
2024-01-30 07:21:31 -0500

    feat: :construction: Created 5th exercise

---
+++ mediaArmonica.py
@@ -0,0 +1,4 @@
+# Desarrolle un programa que calcule la media armónica de una secuencia de números.
+
+# El programa primero debe preguntar al usuario cuántos números ingresará. A continuación, pedirá al usuario que ingrese cada uno de los n
+#  números reales. Finalmente, el programa mostrará el resultado.
