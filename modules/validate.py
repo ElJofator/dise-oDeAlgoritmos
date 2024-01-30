a5b2be33a6344504d9a9e773b0141493a6b71f4d
ElJofator <79776204+ElJofator@users.noreply.github.com>
2024-01-30 07:20:17 -0500

    feat: :sparkles: Added validate for float numbers
    Function that validates if a given number is an integer or float

---
+++ modules/validate.py
@@ -0,0 +1,21 @@
+def validar(numero):                            #Recibe el número como string
+    cantidad = len(numero)
+    if(cantidad > 0):
+        if(numero[0] in "-+" and cantidad > 1): #Retira el signo si comienza con uno 
+            numero = numero[1:]
+            cantidad -= 1
+        punto = False                           #Bandera que verifica si el número tiene punto
+        digitos = "0123456789"
+        for i in range(cantidad):
+            if(numero[i] in digitos):           #Compara cada caracter con los dígitos del 0 al 9
+                continue
+            elif(numero[i] == "." and i > 0):   #Busca si el caracter actual es un punto
+                if(not punto):                  #Verifica que el número no tenga más de un punto
+                    punto = True
+                else:
+                    return False
+            else:
+                return False
+        return True
+    else:
+        return False
