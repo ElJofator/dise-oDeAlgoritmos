6c140ad6541e75aac9abea0cd207e5aaefee2254
ElJofator <79776204+ElJofator@users.noreply.github.com>
2024-01-30 07:10:35 -0500

    feat: :sparkles: Finished 4th exercise

--- caballoDeAjedrez.py
+++ caballoDeAjedrez.py
@@ -7,3 +7,20 @@ while(not columna.isnumeric()):
  columna = input("Columna: ")
 fila = int(fila)
 columna = int(columna)
+if(1 <= fila <= 8 and 1 <= columna <= 8):
+   mov = [[-2,-1],
+          [-2,1],
+          [-1,-2],
+          [-1,2],
+          [1,-2],
+          [1,2],
+          [2,-1],
+          [2,1]]
+   print("El caballo puede saltar a:")
+   for i in range(len(mov)):
+      newFila = fila - mov[i][0]
+      newColumna = columna - mov[i][1]
+      if(1 <= newFila <= 8 and 1 <= newColumna <= 8):
+         print(f"{newFila} {newColumna}")
+else:
+   print("Posición inválida.")
