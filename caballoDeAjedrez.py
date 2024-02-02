2874af9021e23c2d20edbb1b5ed4ab8baa7b6235
ElJofator <79776204+ElJofator@users.noreply.github.com>
2024-02-02 07:15:35 -0500

    refactor: :adhesive_bandage: Replaced substraction with addition
    Now it adds each value to the rows and columns instead of substracting them, it works the same but prints the results in reverse order

--- caballoDeAjedrez.py
+++ caballoDeAjedrez.py
@@ -23,8 +23,8 @@ if(1 <= fila <= 8 and 1 <= columna <= 8):
           [2,1]]
    print("El caballo puede saltar a:")
    for i in range(len(mov)):
-      newFila = fila - mov[i][0]
-      newColumna = columna - mov[i][1]
+      newFila = fila + mov[i][0]
+      newColumna = columna + mov[i][1]
       if(1 <= newFila <= 8 and 1 <= newColumna <= 8):
          print(f"{newFila} {newColumna}")
 else:
